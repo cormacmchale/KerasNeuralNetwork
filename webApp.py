32a9f2936633baffde02b1923c79fd1a4c2a1bdc
unknown <cormacmchale1@gmail.com>
2019-10-22 15:45:15 +0100

    pixel info being recieved by server

--- webApp.py
+++ webApp.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, json, request
-import numpy as py;
+from flask import Flask, render_template, json, request, jsonify
+import json
+import numpy as py
+import sys
+
 app=Flask(__name__)
 value=py.empty
+
 @app.route("/")
 def firstFlaskPage():
     return render_template("flask.html")
@@ -13,11 +17,20 @@ def addNumberPage():
 @app.route("/drawNumber")
 def drawNumberPage():
     return render_template("drawNumber.html")
+
+@app.route("/makePrediction", methods=['POST'])
 def makePrediction():
-    value = request.args.get('image')
-    if(value.size>100):
-        print(value+ "web app logic")
+    data = request.get_json()
+    value = data['pixelArray']
+    ##possible flow of program
+
+    ##import model here
+    ##pass it the numpy array
+    ##pass prediction back into webpage and return the new view
+
+    return jsonify({'result': value})
     
 ##run the app from the script
+##actually start the web app running
 if __name__ == "__main__":
     app.run(debug = True)
