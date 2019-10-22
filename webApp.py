ca51babea107d0903ceca3109ccb668966073cad
unknown <cormacmchale1@gmail.com>
2019-10-22 11:10:43 +0100

    trying to give array to flask with jQuery

--- webApp.py
+++ webApp.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, request
 import numpy as py;
 app=Flask(__name__)
-
+value=py.empty
 @app.route("/")
 def firstFlaskPage():
     return render_template("flask.html")
@@ -13,7 +13,11 @@ def addNumberPage():
 @app.route("/drawNumber")
 def drawNumberPage():
     return render_template("drawNumber.html")
-
+def makePrediction():
+    value = request.args.get('image')
+    if(value.size>100):
+        print(value+ "web app logic")
+    
 ##run the app from the script
 if __name__ == "__main__":
     app.run(debug = True)
