d0b70fd1c9ffd49a48d5ad0f9523b7b00e423591
unknown <cormacmchale1@gmail.com>
2019-11-05 11:26:08 +0000

    application clean up

--- returnPrediction.py
+++ returnPrediction.py
@@ -1,8 +1,10 @@
 import keras as kr
 import numpy as np
 from keras.models import load_model
-
 def abstractPredic(x, m):
+    #get the model to predict
     prediction = m.predict(x)
+    #get the array of outputs
     getPredict = np.array(prediction[0])
+    #predicted number is the index of highest value
     return np.argmax(getPredict)
--- webApp.py
+++ webApp.py
@@ -1,17 +1,21 @@
+#imports for functionality
 from flask import Flask, render_template, json, request, jsonify
 import numpy as np
 import keras as kr
+#for debugging flask wont allow for regular print
 import sys
+#get model onto server for use
 from keras.models import load_model
+numberRecoq = load_model('savedModel/my_model.h5')
 #thread issue fix
 from returnPrediction import abstractPredic
-numberRecoq = load_model('savedModel/my_model.h5')
+#initialize app
 app=Flask(__name__)
 
 @app.route("/")
 def UI():
     return render_template("drawNumber.html")
-
+#jQuesry needs to be added here
 @app.route("/makePrediction", methods=['POST'])
 def makePrediction():
     #get Json data
@@ -20,13 +24,10 @@ def makePrediction():
     value = data['pixelArray']
     pixelInformation = (np.array(value).reshape(1,784))
     almost = abstractPredic(pixelInformation, numberRecoq)
+    #check prediction
     print(almost, file=sys.stderr)
-    #prediction = numberRecoq.predict(pixelInformation).tolist()
-    #respond to the webPage   
-    #return jsonify({'prediction': 'almost'})
-    #return render_template("drawNumber.html")
     
-##run the app from the script
-##actually start the web app running
+#run the app from the script
+#actually start the web app running
 if __name__ == "__main__":
-    app.run(debug = True, threaded = False)
+    app.run(debug = True)
