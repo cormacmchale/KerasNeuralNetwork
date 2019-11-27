e512b5cf0a8f83ef6686cdc96504fd164314c0a7
unknown <cormacmchale1@gmail.com>
2019-11-27 11:21:50 +0000

    finished

--- static/formatImage.js
+++ static/formatImage.js
@@ -10,7 +10,7 @@ h = canvas.height;
 //adtaped from = https://stackoverflow.com/questions/18796921/passing-javascript-array-to-flask
 //variables for teh drawing functionality
 var canvas, ctx, flag = false, prevX = 0, currX = 0, prevY = 0, currY = 0, dot_flag = false;
-var x = "black", y = 3;
+var x = "black", y = 2.5;
 //get the canvas and add the events
 init()
 function init() {
@@ -50,8 +50,8 @@ function findxy(res, e) {
         if (dot_flag) {
             ctx.beginPath();
             ctx.fillStyle = x;
-            //thickness = 1.65 seems to have goo results
-            ctx.fillRect(currX, currY, 3, 3);
+            //thickness
+            ctx.fillRect(currX, currY, 2.5, 2.5);
             ctx.closePath();
             dot_flag = false;
         }
